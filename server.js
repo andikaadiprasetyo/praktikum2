059293ef90cce7afb3b15d56cb20e1b5d8f8979c
andikaadiprasetyo <andika.adiprasetyo@gmail.com>
2022-01-04 15:47:56 +0700

    module2

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+
+const configDatabase = require("./dbConfig/database.js");
+const todo = require("./routes/todo.routes.js");
+
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+//connecting to the mongodb database
+configDatabase();
+
+app.use(cors({ origin: true, credentials: true }));
+
+// add the middlewares
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) =>
+  res.send("Hello there!! Cheers !! The server is up and running")
+);
+
+// using our routes
+app.use("/api/todo", todo);
+
+
+// listen
+app.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+);
